8be2f1ae6759dc57755dfa96d487e8a4389bf89f
wudongpeng <790159670@qq.com>
2022-05-30 16:33:57 +0800

    two commit

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,6 @@ const routes = [
     name: 'Home',
     component: Home
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = new VueRouter({
